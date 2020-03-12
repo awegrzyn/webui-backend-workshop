4d5fe91c189a48763213abb3ed1d9e19d8e35c5d
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-03-12 12:36:16 +0100

    mocha test

---
+++ 6-test/test-index.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const http = require('http');
+const url = require('url');
+
+describe('HTTP', () => {
+
+ it('GET the "/"', (done) => {
+    http.get('http://localhost:8080/',
+      (res) => {
+        assert.strictEqual(res.statusCode, 302);
+        console.log(res.data);
+        const parsedUrl = new url.URL(res.headers.location, 'http://localhost');
+        parsedUrl.searchParams.has('personid');
+        parsedUrl.searchParams.has('name');
+        parsedUrl.searchParams.has('token');
+        done();
+      }
+    );
+  });
+
+  it('GET /hi should respond 200/JSON', (done) => {
+    http.get('http://localhost:8080/api/hi',
+      (res) => {
+        assert.strictEqual(res.statusCode, 200);
+        let rawData = '';
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+        res.on('end', () => {
+          const parsedData = JSON.parse(rawData);
+          assert.strictEqual(parsedData.message, 'hi');
+          done();
+        });
+      }
+    );
+  });
+});
